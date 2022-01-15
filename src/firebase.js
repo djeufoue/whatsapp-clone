e55546270550198daa22a3f81f5096a87263e377
Djeufoue <pablodjeufoue@gmail.com>
2022-01-15 17:05:49 +0100

    npx create react App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDhlcAkAy4iutPbKbp_qdLRJ1djtfAbJtU",
+    authDomain: "whats-app-clone-6f90d.firebaseapp.com",
+    projectId: "whats-app-clone-6f90d",
+    storageBucket: "whats-app-clone-6f90d.appspot.com",
+    messagingSenderId: "943642447483",
+    appId: "1:943642447483:web:34b5de9726c905ab0e1cde",
+    measurementId: "G-PBKM8CG503"
+  }; 
