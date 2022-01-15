84092c5f029a0c1ffe0fb3735b79fd7139546b0b
Djeufoue <pablodjeufoue@gmail.com>
2022-01-15 22:16:01 +0100

    Implement Avatar, the sidebar for Search....

---
+++ src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Avatar, IconButton, Input} from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./Sidebar.css";
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+         <div className="sidebar_header">
+          <Avatar />
+          <div className="sidebar_headerRight">
+              <IconButton>
+                <DonutLargeIcon />
+              </IconButton>
+
+              <IconButton>
+                <ChatIcon />
+              </IconButton>
+              
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            
+          </div>
+         </div>
+         
+         <div className="sidebar_search">
+           <div className="sidebar_searchContainer">
+              <SearchOutlined />
+              <input placeholder="Search or start new chat" type="text"/>
+           </div>
+         </div>
+
+         <div className="sidebar_chats">
+            <SidebarChat />  
+            <SidebarChat />
+            <SidebarChat />             
+            <SidebarChat />
+         </div>
+        </div>
+    )
+}
+
+export default Sidebar
