210ef7fca71697472b157097998c3c901570c8e7
Djeufoue <pablodjeufoue@gmail.com>
2022-01-16 17:23:13 +0100

    Implement & style a seed to display random avatar

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './SidebarChat.css';
+import { Avatar } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import "./SidebarChat.css";
 
 function SidebarChat() {
+    
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+       setSeed(Math.floor(Math.random() * 5000))
+    }, [])
+    
     return (
-        <div>
-           <h1>Hey!</h1>
+        <div className="sidebarChat">
+           <Avatar src = {`https://avatars.dicebear.com/api/human/${seed}.svg `} />
+           <div className="sidebarChat_info">
+               <h2>Room name</h2>
+               <p>Last message..</p>
+           </div>
         </div>
-    )
+    );
 }
 
 export default SidebarChat
