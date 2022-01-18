462a115be586bb4f66d61e45cc8ce24f94a2c487
Djeufoue <pablodjeufoue@gmail.com>
2022-01-18 15:12:05 +0100

    Start setting up the firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDhlcAkAy4iutPbKbp_qdLRJ1djtfAbJtU",
     authDomain: "whats-app-clone-6f90d.firebaseapp.com",
@@ -8,3 +9,11 @@ const firebaseConfig = {
     appId: "1:943642447483:web:34b5de9726c905ab0e1cde",
     measurementId: "G-PBKM8CG503"
   }; 
+
+  const firebaseapp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseapp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
