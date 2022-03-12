c9da2085202c32301807c94616bcacbff40cd2f6
Djeufoue <pablodjeufoue@gmail.com>
2022-03-12 18:23:59 +0100

    Do dispatch of the user upon sign-in

--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './Login.css';
 import { Button } from '@material-ui/core';
 import { auth,provider } from './firebase';
+import {useStateValue} from './StateProvider';
+import {actionTypes} from './reducer';
 
 function Login() {
+    const [{}, dispatch] = useStateValue();
+
     const signIn = ()=>{
-      auth.signInWithPopup(provider).then((result) => console.log(result)).catch((error) => alert(error.message));
+      auth.signInWithPopup(provider).then((result) => {
+          dispatch({
+            type: actionTypes.SET_USER,
+            user: result.user,
+          })
+        }).catch((error) => alert(error.message));
     };
 
   return (
