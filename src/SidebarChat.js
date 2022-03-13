cf8869a452be71c576cb99a6f7c434aad497c00e
Djeufoue <pablodjeufoue@gmail.com>
2022-03-13 15:20:52 +0100

    Implement fetching messages from database

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -4,9 +4,17 @@ import db from "./firebase";
 import "./SidebarChat.css";
 import {Link} from "react-router-dom"
 
-function SidebarChat( {id, name, addNewChat} ) {
-    
+function SidebarChat( {id, name, addNewChat} ) { 
     const [seed, setSeed] = useState('');
+    const [messages, setMessages] = useState("");
+
+    useEffect(()=>{
+     if(id){
+         db.collection('rooms').doc(id).collection('messages').orderBy('timestamp','desc').onSnapshot(snapshot =>(
+            setMessages(snapshot.docs.map(doc => doc.data()))  
+         ))
+     }
+    },[])
 
     useEffect(() => {
        setSeed(Math.floor(Math.random() * 5000))
@@ -29,7 +37,7 @@ function SidebarChat( {id, name, addNewChat} ) {
             <Avatar src = {`https://avatars.dicebear.com/api/human/${seed}.svg `} />
             <div className="sidebarChat_info">
                <h2>{name}</h2>
-               <p>Last message..</p>
+               <p>{messages[0]?.message}</p>
             </div>
          </div>
        </Link>
